e7b6d7ea2a063b045ede7134dc3458ba7793cafa
Antony <fullwz@gmail.com>
2014-09-10 17:31:44 +0200

    shop

---
+++ src/Gbl/SiteVitrineBundle/Controller/CategorieController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Gbl\SiteVitrineBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Buzz\Browser;
+
+class CategorieController extends Controller
+{
+	/**
+	 * @Route("/categories/{name}", name="categories.index")
+	 * @Template()
+	 */
+	public function indexAction($name)
+	{
+		$browser    = new Browser();
+		$produits   = $browser->get('http://localhost/kaliBackOffice/web/app_dev.php/api/products/' . $name);
+		$categorie  = $browser->get('http://localhost/kaliBackOffice/web/app_dev.php/api/categories');
+		
+		$productByCategorie = json_decode($produits->getContent(), true);
+		$categories 		= json_decode($categorie->getContent(), true);
+		
+		return array(
+			'produits' 	 => $productByCategorie,
+			'categories' => $categories	
+		);
+	}	
+}
