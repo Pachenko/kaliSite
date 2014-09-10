e7de1e4374330c54fe4969f65567abc6e65e31de
Antony <fullwz@gmail.com>
2014-09-10 12:08:39 +0200

    Correction de bug, Style formulaire login et inscription

--- src/Gbl/SiteVitrineBundle/Controller/DefaultController.php
+++ src/Gbl/SiteVitrineBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Gbl\SiteVitrineBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Buzz\Browser;
 
 class DefaultController extends Controller
@@ -12,7 +13,6 @@ class DefaultController extends Controller
     /**
      * @Route("/")
      * @Route("/index")
-     * @Template()
      */
     public function indexAction()
     {
@@ -20,9 +20,10 @@ class DefaultController extends Controller
     	//API pour config
     	$response = $browser->get('http://localhost/kaliBackOffice/web/app_dev.php/api/configurations/kaliSiteVitrine');   	
     	//Tableau des infos config
-    	$infoConfig = json_decode($response->getContent(), true);   	    	
+    	$infoConfig = json_decode($response->getContent(), true);  
+    	
     	if (array_key_exists(0, $infoConfig)) {
-    		throw new NotFoundException(sprintf('Configuration inconnue'));
+    		throw new NotFoundHttpException(sprintf('Configuration inconnue'));
     	}   	
     	$id_theme = $infoConfig['theme'];
     	
@@ -30,10 +31,8 @@ class DefaultController extends Controller
     	$responseTheme = $browser->get('http://localhost/kaliBackOffice/web/app_dev.php/api/themes/'.$id_theme);
     	$infoTheme = json_decode($responseTheme->getContent(), true);
     	if (array_key_exists(0, $infoTheme)) {
-    		throw new NotFoundException(sprintf('Thème inconnue'));
+    		throw new NotFoundHttpException(sprintf('Thème inconnue'));
     	}
-    	
-    	//var_dump($infoTheme);die();
     	
     	return $this->render('GblSiteVitrineBundle:Default:index.html.twig', array('theme' => $infoTheme));
     }
